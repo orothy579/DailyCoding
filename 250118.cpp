12a7563d53e341bbb2b51de3118ff6ca01e89bdc
orothy579 <ftsky2050@naver.com>
2025-01-18 23:53:13 +0900

    250118

---
+++ 250118.cpp
@@ -0,0 +1,59 @@
+/*
+  P11399
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  int n ;
+  cin >> n ; 
+  vector<int> a(n,0);
+  vector<int> s(n,0);
+
+  for(int i = 0 ; i < n ; i++)
+  {
+    cin >> a[i];
+  }
+
+  for(int i = 1; i< n; i++)
+  {
+    int ip = i ;
+    int iv = a[i];
+    for(int j = i-1 ; j >= 0 ; j--)
+    {
+      if(a[j] < a[i])
+      {
+        ip = j + 1;
+        break;
+      }
+      if ( j == 0 )
+      {
+        ip = 0;
+      }
+    }
+    for(int j = i; j > ip ; j--)
+    {
+      a[i] = a[j-1];
+    }
+    a[ip] =iv;
+  }
+  s[0] = a[0];
+
+  for(int i = 1; i < n ; i++)
+  {
+    s[i] = s[i-1] + a[i];
+  }
+
+  int sum = 0 ;
+
+  for(int i = 0; i < n ; i++)
+  {
+    sum += s[i];
+  }
+
+  cout << sum;
+
+}
