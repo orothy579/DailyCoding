419c0cf0269af965a43510c38d168b0527454214
orothy579 <ftsky2050@naver.com>
2025-03-11 18:11:30 +0900

    250311

--- 250311.cpp
+++ 250311.cpp
@@ -3,21 +3,31 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
   int n, k;
   cin >> n >> k;
-  vector<int> a;
 
-  // n / i
-  for (int i = 1; i < n; i++)
+  vector<int> a; // 약수를 저장할 벡터
+
+  // 1부터 n까지 반복하여 약수 찾기
+  for (int i = 1; i <= n; i++)
   {
-    if (n / i == 0)
-    {
+    if (n % i == 0)
+    { // i가 n의 약수이면
       a.push_back(i);
     }
-    // i를 기억하기, i가 곧 약수
-    // vector의 인덱스 1부터에 i를 push
-    // vector[k] 미존재시 0출력
   }
+
+  // k번째 약수 출력 (1-based index)
+  if (k <= a.size())
+  {
+    cout << a[k - 1] << endl; // k번째 약수 (벡터는 0-based index)
+  }
+  else
+  {
+    cout << "0" << endl; // k번째 약수가 존재하지 않으면 0 출력
+  }
+
+  return 0;
 }
