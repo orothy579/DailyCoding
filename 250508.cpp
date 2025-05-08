d12c8905147fd10a181a4320ec6808e682524fdd
orothy579 <ftsky2050@naver.com>
2025-05-08 12:47:01 +0900

    250508

---
+++ 250508.cpp
@@ -0,0 +1,18 @@
+// 수 정ㅕ하기 2
+// 2751번
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  int n; // 입력받을 수의 개수
+  vector<int> nums;
+
+  for (int i = 0; i < n; i++)
+  {
+    cin << n;
+  }
+}
